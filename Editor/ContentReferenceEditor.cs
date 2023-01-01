d6727ff33fa0e21ac527f00f6d0e7c32882fd6c3
Logan Shockley <loganv182@gmail.com>
2023-01-01 01:02:13 -0600

    Added a generic content reference editor

--- Editor/ContentReferenceEditor.cs
+++ Editor/ContentReferenceEditor.cs
@@ -52,4 +52,56 @@ namespace VAT.Packaging.Editor
             EditorGUI.EndProperty();
         }
     }
+
+    /// <summary>
+    /// Draws a reference to a generic content asset, which can be toggled between showing the address or the asset itself.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(ContentReferenceT<>), true)]
+    public class ContentReferenceEditorT<T> : PropertyDrawer where T : Object
+    {
+        public ContentT<T> selectedContent = null;
+        public bool isDrawingAddress = false;
+
+        private bool _hasRan = false;
+        private bool _hadValue = false;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var addressProperty = property.FindPropertyRelative("_address").FindPropertyRelative("_id");
+
+            EditorGUI.BeginProperty(position, label, property);
+            position.width -= 24;
+            if (isDrawingAddress)
+            {
+                string result = EditorGUI.TextField(position, label, addressProperty.stringValue);
+                addressProperty.stringValue = result;
+            }
+            else
+            {
+                selectedContent = EditorGUI.ObjectField(position, label, selectedContent, typeof(ContentT<T>), false) as ContentT<T>;
+                if (selectedContent)
+                    addressProperty.stringValue = selectedContent.Address.ID;
+                else if (!_hasRan && AssetPackager.IsReady)
+                {
+                    AssetPackager.Instance.TryGetContent(addressProperty.stringValue, out selectedContent);
+                    _hasRan = true;
+                }
+                else if (_hadValue)
+                {
+                    addressProperty.stringValue = Address.EMPTY;
+                }
+
+                _hadValue = selectedContent != null;
+            }
+
+            int indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            position.x += position.width + 24;
+            position.width = position.height = EditorGUI.GetPropertyHeight(addressProperty);
+            position.x -= position.width;
+            isDrawingAddress = EditorGUI.Toggle(position, isDrawingAddress);
+            EditorGUI.indentLevel = indent;
+            EditorGUI.EndProperty();
+        }
+    }
 }
