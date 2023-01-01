8f6c5901c12adbf20ab4525e369bdf01685b2957
Logan Shockley <loganv182@gmail.com>
2022-12-31 23:17:16 -0600

    Added content references for the new types

---
+++ Runtime/ScriptableObjectContentReference.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace VAT.Packaging {
+    [Serializable]
+    public class ScriptableObjectContentReference : ContentReferenceT<ScriptableObjectContent> {
+    }
+}
